01c9a2b07b0f06b9fb247c914e7f20a47bea957d
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-23 12:50:27 +1000

    task 0

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+number = 3.144444
+is_true = number > 0
+
+print("Language: Python")
+print("Version: 3")
+print(f"Pi approx: {number:.2f}")
+print(f"Computation valid: {is_true}")
