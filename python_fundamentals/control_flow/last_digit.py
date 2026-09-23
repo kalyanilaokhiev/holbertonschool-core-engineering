bf70317bad245a07135d89212179195a9ed7ceac
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-23 14:15:47 +1000

    split the negative and positive digit to keep the negative symbol

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-# takes away negative
-digit = abs(number) % 10
+if number < 0:
+    # takes away negative
+    digit = -(abs(number) % 10)
+else:
+    digit = number % 10
 
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
