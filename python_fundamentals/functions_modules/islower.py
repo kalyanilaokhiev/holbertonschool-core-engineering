95dfa384e5034509730523c1fb91c6b55b0a9937
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-25 17:01:50 +1000

    only returning not printing

--- python_fundamentals/functions_modules/islower.py
+++ python_fundamentals/functions_modules/islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def islower(c):
-    lower = None
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        lower = True
-    else:
-        lower = False
 
-    print(lower)
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
