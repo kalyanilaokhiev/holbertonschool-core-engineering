836806205b38fc1317480a922be55aa860f6d1e3
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-25 17:46:36 +1000

    returning value as well

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -4,6 +4,8 @@ def print_last_digit(number):
     if number < 0:
         last_digit = abs(number) % 10
         print(last_digit, end="")
+        return last_digit
     else:
         last_digit = number % 10
         print(last_digit, end="")
+        return last_digit
