33940955a10b7b99829bf5a86218b783d729e277
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-25 17:44:01 +1000

    forgot the end=

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -3,7 +3,7 @@
 def print_last_digit(number):
     if number < 0:
         last_digit = abs(number) % 10
-        print(last_digit)
+        print(last_digit, end="")
     else:
         last_digit = number % 10
-        print(last_digit)
+        print(last_digit, end="")
