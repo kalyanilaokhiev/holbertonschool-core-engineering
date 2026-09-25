2a42ba593b931edcdfce59d06031a38e76f67bde
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-25 18:14:58 +1000

    accomodating for negative power

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,7 +2,18 @@
 
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result *= a
+    if b < 0:
+        b = abs(b)
+
+        for i in range(b):
+            result *= a
+
+        result = 1 / result
+
+    else:
+        for i in range(b):
+            result *= a
 
     return result
+
+print(pow(10, -2))
