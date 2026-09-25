fd8c688c3905a7eb26a57b760985a265f7a0af0a
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-25 15:44:18 +1000

    removing a print

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    elif i == 99:
-        print(i, end="\n")
+    # to make sure the last number does not have a ,
+    if i == 99:
+        end_symbol = '\n'
     else:
-        print(i, end=", ")
+        end_symbol = ', '
+
+    # adding 0 infront of 0-9
+    if i < 10:
+        print("0{}".format(i), end=end_symbol)
+    else:
+        print(i, end=end_symbol)
