018b3350425a3f73a705c260c33cd9df058ba4cc
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-26 21:34:26 +1000

    changed names

--- python_fundamentals/functions_modules/calculator_1.py
+++ python_fundamentals/functions_modules/calculator_1.py
@@ -3,11 +3,11 @@
 def add(a, b):
     return (a + b)
 
-def subtract(a, b):
+def sub(a, b):
     return (a - b)
 
-def multiply(a, b):
+def mul(a, b):
     return (a * b)
 
-def divide(a, b):
+def div(a, b):
     return (a / b)
