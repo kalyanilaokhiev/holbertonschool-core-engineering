7f9ac058f8e3cf691724a28488cb15846d5e942d
kalyanilaokhiev <14894@holbertonstudents.com>
2026-09-26 18:18:54 +1000

    moving name = main into add

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 import add_0
+
+if __name__ == "__main__":
+    print(add_0.add(1, 2))
--- python_fundamentals/functions_modules/add_0.py
+++ python_fundamentals/functions_modules/add_0.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 def add(a, b):
     return (a + b)
-
-if __name__ == "__main__":
-    print(add(1, 2))
